ad3fa8e5edb2bd1864a0f83ddb7818bb5b92c59d
Victor Mesquini <mesquini@LIVE.COM>
2020-02-05 15:02:35 -0300

    finalizacao do desafio

---
+++ backend/src/app/controllers/RecipientController.js
@@ -0,0 +1,83 @@
+import * as Yup from 'Yup';
+
+import RecipientModel from '../models/Recipient';
+
+class Recipient {
+    async index(req, res) {
+        const { id } = req.body;
+
+        if (id) {
+            const recipient = await RecipientModel.findByPk(id);
+
+            return res.json(recipient);
+        }
+
+        const recipients = await RecipientModel.findAll();
+
+        return res.json(recipients);
+    }
+    async store(req, res) {
+        const shema = Yup.object().shape({
+            name: Yup.string().required(),
+            street: Yup.string().required(),
+            number: Yup.number().required(),
+            state: Yup.string().required(),
+            city: Yup.string().required(),
+            cep: Yup.string().required(),
+        });
+
+        if (!(await shema.isValid(req.body)))
+            return res.status(400).json({ error: 'Validation fails' });
+
+        const {
+            name,
+            street,
+            number,
+            state,
+            city,
+            cep,
+            complements,
+        } = await RecipientModel.create(req.body);
+
+        return res.json({
+            name,
+            street,
+            number,
+            state,
+            city,
+            cep,
+            complements,
+        });
+    }
+    async update(req, res) {
+        const shema = Yup.object().shape({
+            name: Yup.string(),
+            street: Yup.string(),
+            number: Yup.number(),
+            state: Yup.string(),
+            city: Yup.string(),
+            cep: Yup.string(),
+        });
+
+        if (!(await shema.isValid(req.body)))
+            return res.status(400).json({ error: 'Validation fails' });
+
+        const recipient = await RecipientModel.update(req.body, {
+            where: req.params.id,
+        });
+
+        return res.json(recipient);
+    }
+    async delete(req, res) {
+        const recipient = await RecipientModel.findByPk(req.params.id);
+
+        if (!recipient)
+            return res.status(401).json({ error: 'Recipient not found' });
+
+        await RecipientModel.destroy(req.params.id);
+
+        return res.json({ status: 'Recipient delete with success' });
+    }
+}
+
+export default new Recipient();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,23 @@
 import { Router } from 'express';
 
+import RecipientController from './app/controllers/RecipientController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.get('/', (req, res) => {
     return res.json({ ok: true });
 });
+
+routes.post('/session', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.get('/recipient', RecipientController.index);
+routes.post('/recipient', RecipientController.store);
+routes.put('/recipient/:id', RecipientController.update);
+routes.delete('/recipient/:id', RecipientController.delete);
 
 export default routes;
